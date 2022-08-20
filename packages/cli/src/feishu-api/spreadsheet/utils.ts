741ece665cf9b6468f01e00600bdb54b8f7bd9f5
ZeekoZhu <vaezt@outlook.com>
2022-08-20 14:48:04 +0800

    feat(cli): add spreadsheet download command

--- packages/cli/src/feishu-api/spreadsheet/utils.ts
+++ packages/cli/src/feishu-api/spreadsheet/utils.ts
@@ -1,4 +1,4 @@
-import { chain, drop, isEmpty } from 'lodash';
+import _ from 'lodash';
 
 export interface ISpreadsheetUrlParseResult {
   sheetToken: string;
@@ -9,19 +9,20 @@ export const getSheetToken = (urlStr: string): ISpreadsheetUrlParseResult | null
   try {
     const url = new URL(urlStr);
     const sheetId = url.searchParams.get('sheet');
-    const sheetToken = chain(url.pathname).split('/').last().value();
+    const sheetToken = _.chain(url.pathname).split('/').last().value();
     return <ISpreadsheetUrlParseResult>{ sheetToken, sheetId };
   } catch (e) {
+    console.log(e);
     return null;
   }
 };
 
 export const spreadsheetToJson = (sheet: string[][]): any[] => {
-  if (isEmpty(sheet)) {
+  if (_.isEmpty(sheet)) {
     return [];
   }
   const headers = sheet[0];
-  const rows = drop(sheet, 1);
+  const rows = _.drop(sheet, 1);
   return rows.map(row => {
     const obj = {} as Record<string, any>;
     for (let i = 0; i < headers.length; i++) {
--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,3 +1,25 @@
-console.log("cli");
+import chalk from 'chalk';
+import { program } from 'commander';
+import fs from 'fs-extra';
+import path from 'path';
+import { getSheetContent } from './feishu-api/spreadsheet/api';
+import { spreadsheetToJson } from './feishu-api/spreadsheet/utils';
 
-// todo: login feishu and get user access token
+
+const spreadsheetCommand = program.command('spreadsheet');
+spreadsheetCommand.command('download')
+  .description('Downloads the spreadsheet')
+  .argument('<docUrl>', 'The spreadsheet url, e.g. https://test-ciso15gzgfgx.feishu.cn/sheets/shtcnUQqsyPEuWze47yNQs0sQ6d?sheet=699fea')
+  .argument('<output>', 'The output JSON file path')
+  .action(async (docUrl: string, output: string) => {
+    console.log(chalk.yellow('Downloading spreadsheet...'));
+    const content = await getSheetContent(docUrl);
+    const json = spreadsheetToJson(content);
+    const jsonStr = JSON.stringify(json, null, 2);
+    const outputPath = path.resolve(output);
+    fs.ensureFileSync(outputPath);
+    fs.writeFileSync(outputPath, jsonStr, { encoding: 'utf8' });
+    console.log(chalk.green(`Spreadsheet downloaded to ${outputPath}`));
+  });
+
+program.parse(process.argv);
